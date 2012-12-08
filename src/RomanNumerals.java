4777517e5bcc2fd4359a80fd5d3ab405f304271a
oshri ben moshe <oshribm88@gmail.com>
2012-12-08 22:42:32 +0200

    writing the fanction

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,10 +1,20 @@
-
 public class RomanNumerals {
+	private static final String[] RCODE = { "M", "CM", "D", "CD", "C", "XC",
+			"L", "XL", "X", "IX", "V", "IV", "I" };
+	private static final int[] BVAL = { 1000, 900, 500, 400, 100, 90, 50, 40,
+			10, 9, 5, 4, 1 };
 
-	public String toRoman(int nmber) {
-		if (nmber == 1)
-			return "I";
-		return "II";
+	public String toRoman(int number) {
+
+		String roman = ""; 
+		for (int i = 0; i < RCODE.length; i++) {
+			while (number >= BVAL[i]) {
+				number -= BVAL[i];
+				roman += RCODE[i];
+			}
+		}
+		return roman;
+
 	}
 
 }
